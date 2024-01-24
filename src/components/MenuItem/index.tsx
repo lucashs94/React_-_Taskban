79311d9e65b00a96ec7cf037e9fa1d2602dbe24d
lucashs94 <h7.lucas@gmail.com>
2024-01-23 23:18:07 -0300

    MenuItem select by pathName

--- src/components/MenuItem/index.tsx
+++ src/components/MenuItem/index.tsx
@@ -1,18 +1,21 @@
 import { icons } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 
 type IconsProps = {
+  path: string
   name: string
   iconName: string
   selected?: boolean
 }
 
-export default function MenuItem({ name, iconName, selected = false }:IconsProps){
+
+export default function MenuItem({ path, name, iconName, selected = false }:IconsProps){
 
   const LucideIcon = icons[iconName]
 
   return(
-    <div className='flex gap-5 items-center'>
+    <Link to={path} className='flex gap-5 items-center'>
 
       <LucideIcon color={selected ? '#48409E' : '#6F6F6F'} size={20} fill={selected ? '#48409E' : 'transparent'} />
       
@@ -25,6 +28,6 @@ export default function MenuItem({ name, iconName, selected = false }:IconsProps
         {name}
       </h2>
 
-    </div>
+    </Link>
   )
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,7 +1,11 @@
+import { useLocation } from "react-router-dom"
 import MenuItem from "../MenuItem"
 
 
 export default function Nav(){
+
+  const { pathname } = useLocation()
+
   return(
     <nav className='block w-[220px] h-screen bg-white pt-[56px] items-center'>
         
@@ -11,10 +15,10 @@ export default function Nav(){
 
         <section className="flex flex-1 flex-col pt-[57px] px-[33px] gap-[30px]">
 
-          <MenuItem name="Quadro" iconName="LayoutTemplate" selected  />
-          <MenuItem name="Lista" iconName="CalendarRange" />
-          <MenuItem name="Timeline" iconName="List" />
-          <MenuItem name="Calendário" iconName="CalendarRange" />
+          <MenuItem path="/" name="Quadro" iconName="LayoutTemplate" selected={pathname === '/'} />
+          <MenuItem path="/list" name="Lista" iconName="CalendarRange" selected={pathname === '/list'}/>
+          <MenuItem path="/timeline" name="Timeline" iconName="List" selected={pathname === '/timeline'}/>
+          <MenuItem path="/calendar" name="Calendário" iconName="CalendarRange" selected={pathname === '/calendar'}/>
           
         </section>
 
