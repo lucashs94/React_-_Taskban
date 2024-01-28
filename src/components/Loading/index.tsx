8523eeeab13f9571fd2ef8c0c192b024963210dc
lucashs94 <h7.lucas@gmail.com>
2024-01-28 16:55:53 -0300

    Created Loading component for using while the context loads

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Loading(){
+  return(
+    <div className="flex items-center justify-center w-screen h-screen bg-secondary-purple">
+      <p className="font-semibold text-4xl text-primary-purple">
+        Carregando...
+      </p>
+    </div>
+  )
+}
