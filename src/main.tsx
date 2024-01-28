c2ac9f5110a16c5af4002212073a8cd3c4d11341
lucashs94 <h7.lucas@gmail.com>
2024-01-28 16:23:59 -0300

    added contextData provider top main file

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
 import AppRoutes from './routes/index.tsx'
+import { DataContextProvider } from './contexts/DataContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
-    <AppRoutes />
+
+    <DataContextProvider>
+      <AppRoutes />
+    </DataContextProvider>
+    
   </React.StrictMode>,
 )
